c2b7570e678d688d0519a1ff93ccbf3679e26cfe
jredfox <jredfox_@outlook.com>
2025-12-09 16:48:14 -0600

    backup cli

--- docs.py
+++ docs.py
@@ -4,7 +4,7 @@
 -f --full_search same as using -hip (-h -i -p)
 -u update the cache a-sync seach if cache is valid and version string is <= the target
 -o order of search operations 'PATH, INSTALL, HOME', 'P|I|H'
--x disables resolving javac executeable symlinks if javac symlink points from '/usr/bin/java' --> '/usr/lib/jvm/jdk-8u50/bin/javac'
+-x disables resolving javac executeable symlinks if javac symlink points from '/usr/bin/java' --> '/usr/lib/jvm/jdk-8u50/bin/javac' it won't detect a JDK because "/usr/lib" is not part JDK Installation
 -y --resolver uses java -XshowSettings:properties -version 2>&1 to determine JDK_HOME experimental. Used when javac is an actual executeable not a symlink but isn't inside a JDK Installation
 
 jdk-finder.py -t "6-8" -u -hip -o 'INSTALL|HOME|PATH'
