504fd7af72a968e85aeb49eb264420035bf4c592
jredfox <jredfox_@outlook.com>
2025-12-08 20:20:32 -0600

    Update test.py

--- test.py
+++ test.py
@@ -39,27 +39,26 @@ def loadcmd():
     parser = argparse.ArgumentParser(add_help=False)
     parser.add_argument('-t','--target', metavar='"1.8."', default=SENTINEL, help='Target')
     parser.add_argument('target_path', nargs='?', default=SENTINEL, metavar='"1.8."', help='Target as a Positional Paramater')
-    parser.add_argument('-r','--recurse', action='store_true', help='Deep Recursion')
-    parser.add_argument('-q','--quick', action='store_true', help='Quickly fetches the JDK path from the cache with minimal checks')
-    parser.add_argument('-u','--update', action='store_true', help='Update Used with -q in order to update the cache a-sync if -q succeeds')
-    parser.add_argument('-p','--path', action='store_true', help='PATH search only!')
+    parser.add_argument('-r','--recurse', action='store_true', default=SENTINEL, help='Deep Recursion')
+    parser.add_argument('-q','--quick', action='store_true', default=SENTINEL, help='Quickly fetches the JDK path from the cache with minimal checks')
+    parser.add_argument('-u','--update', action='store_true', default=SENTINEL, help='Update Used with -q in order to update the cache a-sync if -q succeeds')
+    parser.add_argument('-p','--path', action='store_true', default=SENTINEL, help='PATH search only!')
     parser.add_argument('-f','--path_first', default=SENTINEL, metavar='TRUE|FALSE', help='Search PATH first before looking in known JDK Installs!')
-    parser.add_argument('-h','--home', action='store_true', help='Search for home & local JDK Installs by the user!')
-    parser.add_argument('-m','--mac_path', action='store_true', help='Search for official macOS JDK Installs!')
-    parser.add_argument('-n','--non_extensive', action='store_true', help='Search for Standard JDK Installs on the linux paths!')
-    parser.add_argument('-e','--exact', action='store_true', help='JDK Version String Must Match Exactly this argument!')
-    parser.add_argument('-a','--all', action='store_true', help='Search for all Applicable JDK Installs not just the first one found!')
+    parser.add_argument('-h','--home', action='store_true', default=SENTINEL, help='Search for home & local JDK Installs by the user!')
+    parser.add_argument('-m','--mac_path', action='store_true', default=SENTINEL, help='Search for official macOS JDK Installs!')
+    parser.add_argument('-n','--non_extensive', action='store_true', default=SENTINEL, help='Search for Standard JDK Installs on the linux paths!')
+    parser.add_argument('-e','--exact', action='store_true', default=SENTINEL, help='JDK Version String Must Match Exactly this argument!')
+    parser.add_argument('-a','--all', action='store_true', default=SENTINEL, help='Search for all Applicable JDK Installs not just the first one found!')
     parser.add_argument('-v','--value_type', default=SENTINEL, metavar='JDK|JRE|ANY', help='JDK Value Install Types')
     parser.add_argument('-x','--resolve_javac', default=SENTINEL, metavar='TRUE|FALSE', help='Resolve Symbolic Links(Symlinks) of the javac executeable!')
-    parser.add_argument('-c','--config_load', action='store_true', help='Config Overrides CLI flags that have not been populated yet! Normally the config only loads without any Command line(CLI) flags.')
-    parser.add_argument('--no_path', action='store_true', help="Don't Search the PATH only known JDK Installs!")
-    parser.add_argument('--help', action='help', help='Show this help message and exit')
+    parser.add_argument('-c','--config_load', action='store_true', default=SENTINEL, help='Config Overrides CLI flags that have not been populated yet! Normally the config only loads without any Command line(CLI) flags.')
+    parser.add_argument('--no_path', action='store_true', default=SENTINEL, help="Don't Search the PATH only known JDK Installs!")
+    parser.add_argument('--help', action='help', default=SENTINEL, help='Show this help message and exit')
 
     args = parser.parse_args()
     for name, value in vars(args).items():
-        globals()['f_' + name] = value
-        isb = isinstance(value, bool)
-        if (not value == SENTINEL and not isb) or (isb and value):
+        if not value == SENTINEL:
+            globals()['f_' + name] = value
             flags.append(name)
 
     #Correct Flags Either from being SENTINEL or not being boolean
@@ -75,11 +74,14 @@ def loadcmd():
     if f_value_type == SENTINEL:
         f_value_type = 'JDK'
     f_resolve_javac = False if f_resolve_javac == SENTINEL else str(f_resolve_javac).lower().startswith('t')
-
     return (not f_config_load)
 
 loadcmd()
 
+for f in flags:
+    print(f)
+
+sys.exit(0)
 for k, v in globals().items():
     if not k.startswith('__') and (not '<' in k) and (not '<' in str(v)):
         print(str(k) + ' "' + str(v) + '"')
