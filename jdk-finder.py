5f6245a650e743aa117c4c991b77a563ff62191c
jredfox <jredfox_@outlook.com>
2025-12-10 16:21:44 -0600

    Update jdk-finder.py

--- jdk-finder.py
+++ jdk-finder.py
@@ -14,6 +14,7 @@ import subprocess
 ###################
 # TODONOW:
 # - re-check config and parse method
+# - cli api look into --recurse_PATH & --recurse_paths
 # - make macOS work
 # - make windows work
 #
