fd30517006c30c9602e9723f70419866f36a2f71
jredfox <jredfox_@outlook.com>
2025-12-09 21:21:04 -0600

    Update test.py

--- test.py
+++ test.py
@@ -14,13 +14,13 @@ recurse = False
 quick = False
 update = False
 clean_cache = False
-f_all = False
+srch_all = False
 search = ''
 intensity = ''
 paths = ''
 application_bundle = ''
 resolver = ''
-config = False
+config_load = False
 home = False
 
 flags = []
@@ -32,7 +32,7 @@ def loadcmd():
     if len(sys.argv) < 2:
         return False
     #Define Global Vars getting edited
-    global target, recurse, quick, update, clean_cache, f_all, search, intensity, paths, application_bundle, resolver, config
+    global target, recurse, quick, update, clean_cache, srch_all, search, intensity, paths, application_bundle, resolver, config_load
     #Parse Command Line Args
     SENTINEL = object()
     parser = argparse.ArgumentParser(add_help=False)
@@ -42,13 +42,13 @@ def loadcmd():
     parser.add_argument('-q','--quick', action='store_true', default=SENTINEL, help='Quickly fetches the Java Path from the Cache with minimal checks')
     parser.add_argument('-u','--update', action='store_true', default=SENTINEL, help='Update Java Cache A-SYNC if the cache check succeeds and the version < max')
     parser.add_argument('-k','--clean_cache', action='store_true', default=SENTINEL, help='Cleans the Java Cache which forces a live synchronous Java search!')
-    parser.add_argument('-a','--all', action='store_true', default=SENTINEL, help='Search for all Applicable Java Installs not just the first one found!')
+    parser.add_argument('-a','--all', dest='srch_all', action='store_true', default=SENTINEL, help='Search for all Applicable Java Installs not just the first one found!')
     parser.add_argument('-s','--search', metavar='\'PATH|INSTALLS|HOME|CUSTOM\'', default=SENTINEL, help="Search Operations and Order! Example -s 'PATH|INSTALLS|HOME' Example 2: -s '*' Says to search all types and use the normal search order")
     parser.add_argument('-i','--intensity', metavar='NORMAL|MIN|OS', default=SENTINEL, help='Search Intensity where Min does minimal non extensive searches based on the search operations and OS. OS Searches Standard Official Java Installation Paths only')
     parser.add_argument('-p','--paths', metavar='\'Dir;Dir 2\'', default=SENTINEL, help='Search Custom Paths separated by \';\' or \':\' Using glob while working with -r. Replaces /bin with /Contents/Home/bin on macOs. Example: \'JDK/bin;/usr/lib/jvm/*/bin 2:~/.jdks\'. If used with -s must contain \'CUSTOM\' in the search')
     parser.add_argument('-b','--application_bundle', default=SENTINEL, metavar='JDK|JRE|ANY|*', help='Java Application Bundle Types')
     parser.add_argument('-x','--resolver', default=SENTINEL, metavar='\'SYMLINK|COMMAND|NONE\'', help="Resolve the actual path of the javac executeable! Examples: -x 'SYMLINK|COMMAND', -x '*'")
-    parser.add_argument('-c','--config', action='store_true', default=SENTINEL, help='Configuration Values Are Used If the CLI Has not overridden them!')
+    parser.add_argument('-c','--config', dest='config_load', action='store_true', default=SENTINEL, help='Configuration Values Are Used If the CLI Has not overridden them!')
     parser.add_argument('--help', action='help', help='Show this help message and exit')
 
     args = parser.parse_args()
@@ -57,7 +57,6 @@ def loadcmd():
             globals()[name] = value
             flags.append(name)
             print(name)
-    sys.exit(0)
 
     #Correct Target & Flags
     if 'target_path' in flags:
@@ -66,11 +65,7 @@ def loadcmd():
         target = args.target_path
         flags.append('target')
 
-    f_path_first = str(f_path_first).lower().startswith('t')
-    f_value_type = str(f_value_type)
-    f_resolve_javac = str(f_resolve_javac).lower().startswith('t')
-
-    return (not f_config_load)
+    return (not config_load)
 
 loadcmd()
 
