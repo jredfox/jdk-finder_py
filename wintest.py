c25db8e067d47e253544dcbb6d591e8b738fba60
jredfox <jredfox_@outlook.com>
2025-12-12 23:07:58 -0600

    Update wintest.py

--- wintest.py
+++ wintest.py
@@ -7,7 +7,7 @@ isWindows = True
 if isWindows:
     if sys.version_info[0] >= 3:
         unicode = str
-    from ctypes import windll, wintypes, create_unicode_buffer
+    from ctypes import windll, wintypes, create_unicode_buffer, c_void_p, byref
     kernel32 = windll.kernel32
     INVALID_HANDLE_VALUE = -1
     FILE_READ_EA = 8
@@ -15,6 +15,24 @@ if isWindows:
     FILE_SHARE_ALL = 0x1 | 0x2 | 0x4  # FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE
     OPEN_EXISTING = 3
     FILE_FLAG_BACKUP_SEMANTICS = 0x02000000
+
+    class NoWOW64:
+        _disable = kernel32.Wow64DisableWow64FsRedirection
+        _revert =  kernel32.Wow64RevertWow64FsRedirection
+
+        def __enter__(self):
+            self.old_value = c_void_p()
+            self.success = self._disable(byref(self.old_value))
+
+        def __exit__(self, type, value, traceback):
+            if self.success:
+                self._revert(self.old_value)
+else:
+    class NoWOW64:
+        def __enter__(self):
+            pass
+        def __exit__(self, type, value, traceback):
+            pass
 
 def realpathw(path):
     path = os.path.abspath(path)
@@ -51,8 +69,30 @@ def realpathw(path):
     finally:
         kernel32.CloseHandle(hFile)
 
+
+exe = '.exe'
+jdirs = set()
+javas = []
+exes = ("java" + exe, "javac" + exe)
+
+def findjavasw(d):
+    for root, dirs, files in os.walk(d, followlinks=True):
+        # Detect loop or repeated symlink target
+        real = realpathw(root)
+        if real in jdirs:
+            dirs[:] = []  # Don't recurse further
+            continue
+        jdirs.add(real)
+
+        if any(t in files for t in exes):
+            javas.append(real)
+
 start = time.time()
-print(os.path.realpath(r"C:\Users\jredfox\Desktop\test\infloop\infloop\..\dir-link-c"))
-print(realpathw( r"C:\Users\jredfox\Desktop\test\infloop\infloop\..\dir-link-c"))
-print(realpathw(r'\\?\Volume{263eee56-b1c8-408e-991a-8f0b5dae1e4b}\Users\jredfox\Desktop\test'))
-print('end ' + str(time.time() - start))
+with NoWOW64():
+    print(realpathw( r"C:\Users\jredfox\Desktop\test\infloop\infloop\..\dir-link-c"))
+    print(realpathw(r'\\?\Volume{263eee56-b1c8-408e-991a-8f0b5dae1e4b}\Users\jredfox\Desktop\test'))
+    print(realpathw(r'C:\Program Files'))
+    print(realpathw(r'C:\Windows\System32'))
+    print(os.path.expandvars('%PROGRAMFILES%'))
+    print(os.path.expandvars(sys.argv[1]))
+    print(wintypes.HANDLE(-1).value)
