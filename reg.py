58f970d01122a913b1adf8c50120b0c05ecbc951
jredfox <jredfox_@outlook.com>
2025-12-22 12:37:33 -0600

    Update reg.py

--- reg.py
+++ reg.py
@@ -8,14 +8,18 @@ VERSION_Q = re.compile(
 VERSION_UQ = re.compile(
     r'(' + VERSION_CORE + r')'
 )
+BUILD_REG = re.compile(
+    r'\bbuild\b', 
+    re.IGNORECASE
+)
 
 #Throws exception one version cannot be parsed
 def get_ver(line):
     m = VERSION_Q.search(line)
     if not m:
-        i = line.find('build')
-        if i > 0:
-            line = line[i:]
+        build = BUILD_REG.search(line)
+        if build:
+            line = line[build.start()+5:]
         return VERSION_UQ.search(line).group(1)
     return m.group(1)
 
@@ -25,7 +29,7 @@ versions = [
     'OpenJDK version \'"1.8.0_471.mix_mode"\' Vendor Version 1.8.0_471-alphawolf010',
     'OpenJDK version ( build 1.7.0_65-b48 )',
     'OpenJDK Runtime Environment Microsoft-11913448 (build 11.0.28+6-LTS)',
-    'OpenJDK 64-Bit Server VM (Alibaba Dragonwell Extended Edition 8.26.25) (build 25.462-b01, mixed mode)'
+    'OpenJDK 64-Bit Server VM (Alibaba Dragonwell Extended Edition 8.26.25) (BUILD 25.462-b01, mixed mode)'
 ]
 
 for v in versions:
